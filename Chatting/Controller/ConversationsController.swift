911e4f5bf1ee72441ed7988730df7a474d6a5521
Victor Uriarte <uriarte0505@gmail.com>
2021-06-29 10:03:57 -0300

    First commit, setting root viewController and navBar

---
+++ Chatting/Controller/ConversationsController.swift
@@ -0,0 +1,23 @@
+//
+//  ConversationsController.swift
+//  Chatting
+//
+//  Created by Victor Uriarte on 29/06/21.
+//
+
+import UIKit
+
+class ConversationsController: UIViewController {
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+    // MARK: = Helpers
+    
+    
+}
---
+++ Chatting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chatting
+//
+//  Created by Victor Uriarte on 29/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

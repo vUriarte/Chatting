82b624fdca2a00ad3b34a74187bc553aeb3cca2d
Victor Uriarte <uriarte0505@gmail.com>
2021-06-30 11:15:01 -0300

    Creating main table view

--- Chatting/Controller/ConversationsController.swift
+++ Chatting/Controller/ConversationsController.swift
@@ -7,8 +7,11 @@
 
 import UIKit
 
+private let reuseIdentifier = "ConversationCell"
 class ConversationsController: UIViewController {
     // MARK: - Properties
+    
+    private let tableView = UITableView()
     
     // MARK: - Lifecycle
     
@@ -27,12 +30,25 @@ class ConversationsController: UIViewController {
     func configureUI() {
         view.backgroundColor = .white
         configureNavigationBar()
+        configureTableView()
     
         
         let image = UIImage(systemName: "person.circle.fill")
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(showProfile))
         
+    }
+    
+    func configureTableView() {
+        tableView.backgroundColor = .white
+        tableView.rowHeight = 80
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.tableFooterView = UIView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        view.addSubview(tableView)
+        tableView.frame = view.frame
     }
     
     func configureNavigationBar() {
@@ -45,7 +61,6 @@ class ConversationsController: UIViewController {
         navigationController?.navigationBar.compactAppearance = appearence
         navigationController?.navigationBar.scrollEdgeAppearance = appearence
         
-        
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.title = "Messages"
         navigationController?.navigationBar.tintColor = .white
@@ -54,8 +69,24 @@ class ConversationsController: UIViewController {
         navigationController?.navigationBar.overrideUserInterfaceStyle = .dark
         
         
-        
+    }
+}
+
+extension ConversationsController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
     }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        cell.textLabel?.text = "Test Cell \(indexPath.row)"
+        return cell
+    }
+}
+
+extension ConversationsController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
     
 }
